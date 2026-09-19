379626c972d17780b943558e69dd1b3892f97c93
GuacuClavijo <guacunoe@hotmail.com>
2026-09-19 00:35:40 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,5 @@
+const CACHE='liberacao-frentes-v1';
+const ASSETS=['./','./index.html','./app.css','./app.js','./app_data.js','./manifest.webmanifest','./icon.svg'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
+self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(resp=>{const copy=resp.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return resp}).catch(()=>caches.match('./index.html')))));
