feec7051720ed46300a779a92e882ccf64c1246d
GuacuClavijo <guacunoe@hotmail.com>
2026-09-19 00:18:37 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,50 @@
-const CACHE='liberacao-frentes-v1';
-const ASSETS=['./','./index.html','./app.css','./app.js','./app_data.js','./manifest.webmanifest','./icon.svg'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
-self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
-self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(resp=>{const copy=resp.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return resp}).catch(()=>caches.match('./index.html')))));
+const CACHE = 'liberacao-frentes-v2';
+
+const ASSETS = [
+  './',
+  './index.html',
+  './app.css',
+  './app.js',
+  './app_data.js',
+  './manifest.webmanifest',
+  './icon.svg',
+  './logo.svg'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE)
+      .then(cache => cache.addAll(ASSETS))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys
+          .filter(key => key !== CACHE)
+          .map(key => caches.delete(key))
+      )
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    fetch(event.request)
+      .then(response => {
+        const copy = response.clone();
+        caches.open(CACHE).then(cache => {
+          cache.put(event.request, copy);
+        });
+        return response;
+      })
+      .catch(() =>
+        caches.match(event.request).then(
+          response => response || caches.match('./index.html')
+        )
+      )
+  );
+});
